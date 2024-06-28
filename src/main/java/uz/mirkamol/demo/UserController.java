6fa1fc2a82902d278515ee8811a19ed070264345
mirkamol <mirkamolasrorov5@gmail.com>
2024-06-29 04:26:47 +0500

    added delete operation for User

--- src/main/java/uz/mirkamol/demo/UserController.java
+++ src/main/java/uz/mirkamol/demo/UserController.java
@@ -2,18 +2,21 @@ package uz.mirkamol.demo;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 @RestController
 @RequiredArgsConstructor
 public class UserController {
 
-     static List<User> userList = new ArrayList<>();
+     static List<User> userList = new ArrayList<>(Arrays.asList(new User("Jahongir", 23), new User("Mirkamol", 18), new User("Sarvar", 19)));
+
+     @GetMapping("/")
+     public String index() {
+         return  "Hello Uzbekistan";
+     }
     @GetMapping("/user")
     public String hello() {
         return userList.toString();
@@ -24,6 +27,15 @@ public class UserController {
         newUser.setName(requestUser.getName());
         newUser.setAge(requestUser.getAge());
         userList.add(newUser);
+        return ResponseEntity.ok(newUser);
+    }
+
+   @DeleteMapping
+    public ResponseEntity<?> deleteUser(@RequestBody User requestUser) {
+        User newUser = new User();
+        newUser.setName(requestUser.getName());
+        newUser.setAge(requestUser.getAge());
+        userList.remove(newUser);
         return ResponseEntity.ok(newUser);
     }
 }
